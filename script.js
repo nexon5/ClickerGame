16fa0bea42c424c6ea767c1ed579fbf033992910
nexon5 <dm.lychahin@gmail.com>
2021-08-25 14:25:35 +0300

    FourtCommit

--- script.js
+++ script.js
@@ -2,15 +2,33 @@ let btn = document.querySelector("button");
 let value = document.querySelector(".value");
 let out = document.querySelector(".output");
 let fon = document.querySelector(".content");
+let divHelper = document.querySelector(".am-helper");
+let divScale = document.querySelector(".am-scale");
+
+
+
+
 let x = 6;
 let level = 1;
-let red = 174;
-let green = 237;
-let blue = 237;
+// let red = 174;
+// let green = 237;
+// let blue = 237;
 let score = 0;
 let helper = 0;
-let flag = 0; //flag for Fair function
+let flag = 0;
+let cost = 2000; //flag for Fair function
 //255/60/20
+
+let newDiv = document.createElement("div");
+newDiv.classList.add("end");
+let btnRel = document.createElement("button");
+btnRel.classList.add("reload-btn");
+btnRel.textContent = "Начать заново";
+btnRel.onclick = () => {
+    location.reload();
+}
+
+
 
 /*function changeTheme() {
     if (x > 0) {
@@ -24,10 +42,11 @@ let flag = 0; //flag for Fair function
 
 btn.onclick = () => {
     if (x < 56) {
-        x = x + 5 + helper - level;//formula
+        x = x + 5 + helper - level * 2;//formula
         value.style.width = x + "vw";
         score += x;
         out.innerHTML = `Your level is ${level}!!!<br> Your score is ${score}!!!`;
+        CheckHelps();
     } else {
         x = 6;
         level++
@@ -37,22 +56,47 @@ btn.onclick = () => {
 }
 function Fair() {
     if (flag == 0) {
-        if (level >= 1) {
+        if (level >= 0) {
             let timerId = setInterval(() => {
                 x--;
                 console.log("Rabota intervala " + x);
                 value.style.width = x + "vw";
                 flag = 1;
                 if (x <= 0) {//End of game
-                    let newDiv = document.createElement("div");
-                    newDiv.classList.add("end");
                     newDiv.innerHTML = `<h1 class="secondH1">Игра окончена</h1><p>К сожалению Ваша игра окончена и бункер сгорел до тла.
                     Однако вы долго держались.</p><br><center> Ваш счёт:  ${score}<br> Ваш Уровень:  ${level}</center> `;
                     document.body.append(newDiv);
-                    console.log("Выполнение");
+                    newDiv.append(btnRel);
                     clearInterval(timerId);
                 }
             }, 1000);
         }
+    } else return false;
+}
+
+
+
+
+divHelper.innerHTML = `<h2>Helper</h2>
+<p>Buy for increcing liter per one pushing<br><br> Press for upgrading <br>Cost: ${cost} </p>`;
+divScale.innerHTML = `<h2>Scaler</h2>
+<p>Automatical system<br><br> Press for upgrading <br>Cost:  ${cost} </p>`;
+
+function CheckHelps() {
+    if (score > cost) {
+        divHelper.classList.add("active");
+        divScale.classList.add("active");
+        flagHelps = 1;
+    } else false;
+}
+
+divHelper.onclick = () => {
+    if (flagHelps == 1) {
+        score = score - cost;
+        helper += 10;
+        cost += 1000;
+        console.log(cost);
+        flagHelps = 0;
+        divHelper.classList.remove("active");
     } else return false;
 }
